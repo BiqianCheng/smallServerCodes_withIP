9915e1d0a3515c659070465a2453b9de5b95b43d
Biqian Cheng <bchen158@ucr.edu>
2022-06-29 10:23:28 -0700

    add driver outside of server code

--- A-server.c
+++ A-server.c
@@ -7,22 +7,30 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
-
+#include <klee/klee.h>
 #define PORT 55555
 #define MAXLINE 1024
 
 // Driver code
-int main() {
+void driver()
+{
+    char reply[MAXLINE];
+    // char *reply = malloc(MAXLINE);
+    klee_make_symbolic(&reply, sizeof(reply), "reply");
+}
+
+// server logic
+void serverLogic(char buffer[], int sockfd, struct sockaddr_in servaddr, struct sockaddr_in cliaddr, char reply[])
+{
     printf("Part A: Bandwidth bug on only certain path. Only triggers the bug with client sending a single char 'a'. \n\n-----------------\nphp client.php a\n------------------\n");
-    int sockfd;
-    char buffer[MAXLINE];
+
     char largeReply[] =
         "a1b2c3d4e5f6g7h8i9j10a1b2c3d4e5f6g7h8i9j10a1b2c3d4e5f6g7h8i9j10a1b2c3d4e5f6g7h8i9j10a1b2c3d4e5f6g7h8i9j10a1b2c3d4e5f6g7h8i9j10a1b2c3d4e5f6g7h8i9j10a1b2c3d4e5f6g7h8i9j10";
     char smallReply[] = "5";
-    struct sockaddr_in servaddr, cliaddr;
 
     // Creating socket file descriptor
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
         perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
@@ -31,37 +39,55 @@ int main() {
     memset(&cliaddr, 0, sizeof(cliaddr));
 
     // Filling server information
-    servaddr.sin_family = AF_INET;  // IPv4, match the socket() call
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // bind to 169.235.26.114 local IP address
-    servaddr.sin_port = htons(PORT); //specify port to listen on
+    servaddr.sin_family = AF_INET; // IPv4
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(PORT);
 
     // Bind the socket with the server address
-    if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) <
-        0) {
+    if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
 
-    int len, n, o;
+    unsigned int len, n, o;
 
-    len = sizeof(cliaddr);  // len is value/resuslt
-
-    while (1) {
+    len = sizeof(cliaddr);
+    n = recvfrom(sockfd, (char *)buffer, MAXLINE * sizeof(char), MSG_WAITALL,
+                 (struct sockaddr *)&cliaddr, &len);
+    // buffer[n] = '\0';
+    while (1)
+    {
         n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL,
                      (struct sockaddr *)&cliaddr, &len);
         buffer[n] = '\0';
         printf("Client : %s\n", buffer);
 
-        if (strcmp(buffer, "a") == 0) {
+        if (strcmp(buffer, "a") == 0)
+        {
             sendto(sockfd, (const char *)largeReply, sizeof(largeReply),
                    MSG_CONFIRM, (const struct sockaddr *)&cliaddr, len);
-        } else {
+        }
+        else
+        {
             sendto(sockfd, (const char *)smallReply, sizeof(smallReply),
                    MSG_CONFIRM, (const struct sockaddr *)&cliaddr, len);
         }
 
         printf("***SENT***\n");
     }
+    printf("\n -------------- \n");
+}
+
+int main()
+{
+    char buffer[MAXLINE];
+    int sockfd;
+    struct sockaddr_in servaddr, cliaddr;
+
+    klee_make_symbolic(&buffer, sizeof(buffer), "buffer");
+    // klee_make_symbolic(&sockfd, sizeof(sockfd), "sockfd");
+    driver(buffer, sockfd, servaddr, cliaddr, reply);
 
     return 0;
 }

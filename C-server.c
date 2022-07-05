0c7ad29595edaceabf535b7f393b67a2e8639ffb
Biqian Cheng <bchen158@ucr.edu>
2022-07-05 10:36:09 -0700

    with amplification features

--- C-server.c
+++ C-server.c
@@ -8,30 +8,34 @@
 #include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
+#include <klee/klee.h>
 
 #define PORT 55555
 #define MAXLINE 1024
 
 // Driver code
-int main() {
+int serverLogic(char buffer[], char reply[])
+{
+    int count = 0;
     printf(
         "Part C: Client sends anything to have the server return data from a "
         "local file WITHOUT TRIGGERS.\n");
     int sockfd;
-    char buffer[MAXLINE];
+
     char largeReply[] =
         "a1b2c3d4e5f6g7h8i9j10a1b2c3d4e5f6g7h8i9j10a1b2c3d4e5f6g7h8i9j10a1b2c3d"
         "4e5f6g7h8i9j10a1b2c3d4e5f6g7h8i9j10a1b2c3d4e5f6g7h8i9j10a1b2c3d4e5f6g7"
         "h8i9j10a1b2c3d4e5f6g7h8i9j10";
     char smallReply[] = "5";
     // char dataStructure[50] = "";
-    char *dataStructure;
-    dataStructure = malloc(sizeof(char));
+    // char *dataStructure;
+    // dataStructure = malloc(sizeof(char));
 
     struct sockaddr_in servaddr, cliaddr;
 
     // Creating socket file descriptor
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
         perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
@@ -40,24 +44,25 @@ int main() {
     memset(&cliaddr, 0, sizeof(cliaddr));
 
     // Filling server information
-    servaddr.sin_family = AF_INET;  // IPv4
+    servaddr.sin_family = AF_INET;                     // IPv4
     servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // bind to 127.0.0.1 local IP address
-    servaddr.sin_port = htons(PORT); //specify port to listen on
+    servaddr.sin_port = htons(PORT);                   // specify port to listen on
 
     // Bind the socket with the server address
-    if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) <
-        0) {
+    if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
 
-    int len, n, o;
+    int n, o;
 
-    len = sizeof(cliaddr);  // len is value/resuslt
+    socklen_t len = sizeof(cliaddr); // len is value/result
 
-    while (1) {
+    while (1)
+    {
         n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL,
-                     (struct sockaddr *)&cliaddr, &len);
+                     (struct sockaddr *)&servaddr, &len);
         buffer[n] = '\0';
         printf("Client : %s\n", buffer);
 
@@ -68,9 +73,32 @@ int main() {
 
         sendto(sockfd, (const char *)fileBuff, strlen(fileBuff), MSG_CONFIRM,
                (const struct sockaddr *)&cliaddr, len);
-
+        memcpy(reply, fileBuff, strlen(fileBuff));
+        count += strlen(fileBuff);
+        // Manually adding amplification features
+        sendto(sockfd, (const char *)fileBuff, strlen(fileBuff), MSG_CONFIRM,
+               (const struct sockaddr *)&cliaddr, len);
+        memcpy(reply + count, fileBuff, strlen(fileBuff));
         printf("***SENT***\n");
     }
 
+    return 0;
+}
+
+int driver()
+{
+    char buffer[MAXLINE];
+    // printf("The size of buffer is %zu\n", sizeof(buffer));
+    char reply[MAXLINE * 1000];
+    klee_make_symbolic(&buffer, sizeof(buffer), "buffer");
+
+    // klee_assume(buffer[MAXLINE - 1] == '\0');
+    klee_make_symbolic(&reply, sizeof(reply), "reply");
+    return serverLogic(buffer, reply);
+}
+
+int main()
+{
+    driver();
     return 0;
 }
